78287f462bc4685f24f1231d285fcf370f9400c9
Quincy van Bommel <quincy@weave.nl>
2021-09-28 15:33:57 +0200

    feat: wait until i18n is initalized before trigger first update

--- src/index.ts
+++ src/index.ts
@@ -4,34 +4,43 @@ type UpdatingElement = {
     connectedCallback(): void
     disconnectedCallback(): void
 
+    performUpdate(): void | Promise<any>
     requestUpdate(...args: any[]): void
 }
 
 export function localized(init?: InitOptions | ((i18next: i18n) => InitOptions)) {
-  return function <T extends { new (...args: any[]): UpdatingElement}>(constructor: T) {
-      return class extends constructor {
-          public languageChangeHandler = () => this.requestUpdate();
+    return function <T extends { new(...args: any[]): UpdatingElement }>(constructor: T) {
+        return class extends constructor {
+            public languageChangeHandler = this.requestUpdate.bind(this);
+            public isI18nInitialized?: Promise<unknown>;
 
-          constructor(...args: any[]) {
-              super(...args);
+            constructor(...args: any[]) {
+                super(...args);
 
-              const initOptions = typeof init === 'function' ? init(i18next) : init;
-              if (initOptions && !i18next.isInitialized) {
-                  i18next.init(initOptions).catch(console.error);
-              }
-          }
+                const initOptions = typeof init === 'function' ? init(i18next) : {...init};
+                if (!this.isI18nInitialized && !i18next.isInitialized) {
+                    this.isI18nInitialized = i18next.init(initOptions);
+                    this.isI18nInitialized.catch(console.warn);
+                }
+            }
 
-          public connectedCallback() {
-              super.connectedCallback();
-              i18next.on('languageChanged', this.languageChangeHandler);
-          }
+            connectedCallback() {
+                super.connectedCallback();
+                i18next.on('languageChanged', this.languageChangeHandler);
+            }
 
-          public disconnectedCallback(): void {
-              super.disconnectedCallback();
-              i18next.off('languageChanged', this.languageChangeHandler);
-          }
-      }
-  }
+            disconnectedCallback(): void {
+                i18next.off('languageChanged', this.languageChangeHandler);
+                super.disconnectedCallback();
+            }
+
+            async performUpdate(): Promise<unknown> {
+                await this.isI18nInitialized;
+
+                return super.performUpdate();
+            }
+        };
+    };
 }
 
 export function t(...args: Parameters<TFunction>): ReturnType<TFunction> {
